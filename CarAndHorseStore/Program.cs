7ab5acdc57bdcc78f2bf4c3ceeb44415ab9f640e
Kamil Ślusarczyk <kamil.slusarczyk@hotmail.com>
2015-03-08 08:53:25 +0100

    Database connection working + database diagram added

---
+++ CarAndHorseStore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarAndHorseStore.Models;
+
+namespace CarAndHorseStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wieslaw = new Horse() {Name = "wieslaw"};
+
+            using (var context = new HorseContext())
+            {
+                context.Horses.Add(wieslaw);
+                context.SaveChanges();
+            }
+
+            Console.WriteLine(wieslaw.Name);
+            Console.ReadKey();
+        }
+    }
+}
